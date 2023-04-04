72bc4104638aa92ca3d6e6c5333a959621249edf
AndreFoulem <timmycasual@icloud.com>
2023-04-04 13:34:15 -0700

    Initial Commit

---
+++ NorthPoleWeather/NorthPoleWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  NorthPoleWeatherApp.swift
+//  NorthPoleWeather
+//
+//  Created by AndreMacBook on 2023-04-04.
+//
+
+import SwiftUI
+
+@main
+struct NorthPoleWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
