ceb9f25757851a93bf8aba407ea5eedb2a21beff
AndreFoulem <timmycasual@icloud.com>
2023-04-06 11:24:24 -0700

    Implement Forecast Card

--- NorthPoleWeather/Views/Details/ForecastCard.swift
+++ NorthPoleWeather/Views/Details/ForecastCard.swift
@@ -10,7 +10,15 @@ import SwiftUI
 struct ForecastCard: View {
   var forecast: Forecast
   var forecastPeriod: ForecastPeriod
-  var isActive: Bool = true
+  var isActive: Bool {
+    if forecastPeriod == ForecastPeriod.hourly {
+      let isThisHour = Calendar.current.isDate(.now, equalTo: forecast.date, toGranularity: .hour)
+      return isThisHour
+    } else {
+      let isToday = Calendar.current.isDate(.now, equalTo: forecast.date, toGranularity: .day)
+      return isToday
+    }
+  }
   
     var body: some View {
       ZStack {
@@ -27,7 +35,30 @@ struct ForecastCard: View {
           }
           .innerShadow(shape: RoundedRectangle(cornerRadius: 30), color: .white.opacity(0.25), lineWidth: 1, offsetX: 0, offsetY: 1, blur: 0, blendMode: .overlay)
         
-        
+        // MARK: Content
+        VStack(spacing: 16) {
+          //MARK: Forecast time and date
+          Text(forecast.date, format: forecastPeriod == ForecastPeriod.hourly ? .dateTime.hour() : .dateTime.weekday())
+            .font(.caption.weight(.semibold))
+          
+          VStack(spacing: -4) {
+            
+            // MARK: Forecast Small Icon
+            Image("\(forecast.icon) small")
+            
+            // MARK: Forecast Probability
+            Text(forecast.probability, format: .percent)
+              .font(.footnote.weight(.semibold))
+              .foregroundColor(Color.probabilityText)
+              .opacity(forecast.probability > 0 ? 1 : 0)
+          }
+          .frame(height: 42)
+            // MARK: Forecast Temperature
+          Text("\(forecast.temperature)°")
+        }
+        .padding(.horizontal, 8)
+        .padding(.vertical, 16)
+        .frame(width: 60, height: 146)
         
         
       }
@@ -36,6 +67,6 @@ struct ForecastCard: View {
 
 struct ForecastCard_Previews: PreviewProvider {
     static var previews: some View {
-      ForecastCard(forecast: Forecast.hourly[0], forecastPeriod: .hourly)
+      ForecastCard(forecast: Forecast.hourly[5], forecastPeriod: .hourly)
     }
 }
